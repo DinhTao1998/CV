419caf0fd6d81ba6c7d33299830c84f2f747f24b
ChuongTranKT <vanchuongktt1998@gmail.com>
2019-05-09 22:56:52 +0700

    fix header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,66 +1,60 @@
 <div class="header">
     <div class="container">
-        <div class="row-header">
-            <!-- <div class="col-4">
-                <div class="logo">
-                    <a href="public/CV/jackson/index.html">
-                        <img alt="" src="resources/images/akari.png"/>
+        <div class="navbar-logo">
+            <a href="index.html">
+                <img src="CV/jackson/images/akari.png" style="width: 155px;"/>
+            </a>
+        </div>
+        <!-- <div class="wrap"> -->
+        <div class="menu">
+            <ul class="ul-menu">
+                <li>
+                    <a href="#">
+                        <i class="icon-home">
+                        </i>
+                        Home
                     </a>
-                </div>
-            </div> -->
-<div class="navbar-logo">
-         <a href="index.html"><img data-0="width:155px;" data-300=" width:120px;" src="CV/jackson/images/akari.png" alt="" class="skrollable skrollable-between" style="width: 155px;"></a>
-       </div>
-            <div class="wrap">
-                <div class="menu">
-                    <ul class="ul-menu">
-                        <li>
-                            <a href="#">
-                                <i class="icon-home">
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon-user2">
+                        </i>
+                        CV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon-opera">
+                        </i>
+                        University
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon-opera">
+                        </i>
+                        Company
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <section class="filter-job">
+                <div class="filter-head">
+                    <div class="head head-1">
+                        <div class="autocomplete input_search">
+                            <span class="icon-before">
+                                <i class="li-briefcase">
                                 </i>
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="icon-user2">
-                                </i>
-                                CV
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="icon-opera">
-                                </i>
-                                University
-                            </a>
-                        </li>
-                         <li>
-                            <a href="#">
-                                <i class="icon-opera">
-                                </i>
-                                Company
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="tab-content">
-                    <section class="filter-job">
-                        <div class="filter-head">
-                            <div class="head head-1">
-                                <div class="autocomplete input_search">
-                                    <span class="icon-before">
-                                        <i class="li-briefcase">
-                                        </i>
-                                    </span>
-                                    <input autocomplete="off" class="form-control" placeholder="Nhập tiêu đề hồ sơ, vị trí, địa điểm làm việc..." value="">
-                                        <!-- -->
-                                        <!-- -->
-                                    </input>
-                                </div>
-                            </div>
-                            <!-- <div class="head head-2">
-                        <div class="select input_search">
+                            </span>
+                            <input autocomplete="off" class="form-control" placeholder="Nhập tiêu đề hồ sơ, vị trí, địa điểm làm việc..." value="">
+                                <!-- -->
+                                <!-- -->
+                            </input>
+                        </div>
+                    </div>
+                    <div class="head head-2">
+                        <div class="mw_select input_search">
                             <span class="icon-before">
                                 <i class="li-hammer-wrench">
                                 </i>
@@ -70,21 +64,21 @@
                                     <i class="el-icon-arrow-down fs-12">
                                     </i>
                                 </span>
-                                
+                                <!-- -->
                             </input>
                         </div>
-                    </div> -->
-                            <div class="head head-3">
-                                <button class="btn btn-search">
-                                    <i class="icon-search4">
-                                    </i>
-                                    Tìm kiếm
-                                </button>
-                            </div>
-                        </div>
-                    </section>
+                    </div>
+                    <div class="head head-3">
+                        <button class="btn btn-search">
+                            <i class="icon-search4">
+                            </i>
+                            Tìm kiếm
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
+        <!-- </div> -->
+        <!-- </div> -->
     </div>
 </div>
